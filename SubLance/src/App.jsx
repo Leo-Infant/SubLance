4945f95a9cb6551cbdbdcd95c9499abed244a045
Leo-Infant <leoinfleo@gmail.com>
2025-01-14 11:13:33 +0530

    Initial Git

---
+++ SubLance/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import LoginForm from './Components/LoginForm';
+// import AboutUs from './Components/AboutUS';
+function App() {
+
+  return (
+    <>
+     <LoginForm/>
+    </>
+  )
+}
+
+export default App
---
+++ SubLance/src/Components/LoginForm.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import '../index.css';
+import Logo from '../assets/Logo.png'
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="login-container">
+        <div className="logo-container">
+          <img src={Logo} alt="Logo" />
+          <h1>SubLance</h1>
+        </div>
+        <div>
+        <h2>Member Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <input
+              type="email"
+              placeholder="example@gmail.com"
+              value={email}
+              onChange={handleEmailChange}
+            />
+            <span className="input-icon">@</span>
+          </div>
+          <div className="input-group">
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <span className="input-icon">🔒</span>
+          </div>
+          <button type="submit">Login</button>
+        </form>
+        <div className="options">
+          <label>
+            <a href="#">Forgot Password?</a> | <a href="#">Join with Us</a>
+          </label >
+
+        </div>
+        </div>
+    </div>
+  );
+}
+
+export default Login;
